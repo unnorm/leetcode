58b8c5341e8af47728c35e10e773363d7413e067
arrebole <326093153@qq.com>
2021-07-09 00:18:07 +0800

    solve/1.two-sum: 哈希记忆法求两数之和为目标数
    link: #1

---
+++ test/1.two-sum/twoSum.test.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+const assert = require('assert');
+const { twoSum } = require('../../src/1.two-sum/twoSum');
+
+function testTwoSum() {
+    const taget = 6;
+    const nums = [3,2,4];
+
+    const result = twoSum(nums, taget);
+    assert.strictEqual(
+        result.map(v => nums[v]).reduce((p, c) => p + c),
+        taget,
+    )
+}
+
+testTwoSum();
